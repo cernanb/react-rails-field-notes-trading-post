187e96f1d75620098823cbe7b6fea7060899c4ce
Cernan Bernardo <cernanb@gmail.com>
2017-03-10 19:09:34 -0800

    changed http method for login and signup routes to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/signup', to: 'api/v1/controllers#signup'
-      get '/login', to: 'api/v1/controllers#login'
+      post '/signup', to: 'api/v1/controllers#signup'
+      post '/login', to: 'api/v1/controllers#login'
     end
   end
 end
