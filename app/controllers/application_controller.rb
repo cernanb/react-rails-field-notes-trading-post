ddc09d4ce1d000a70628008edf09050a2837eac3
Cernan Bernardo <cernanb@gmail.com>
2017-03-12 00:12:28 -0800

    current user and logged in helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(auth.first["user"]["id"])
+  end
+
+  private
+    def token
+      request.env["HTTP_AUTHORIZATION"]
+    end
+
+    def auth
+      Auth.decode_token(token)
+    end
+
 end
