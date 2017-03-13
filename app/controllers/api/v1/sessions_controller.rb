44ce06e8d580a4548e623408f1557e766ee22474
Cernan Bernardo <cernanb@gmail.com>
2017-03-12 22:27:35 -0700

    updated object that is rendered on successful login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::SessionsController < ApplicationController
   def login
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
-      render json: { token: Auth.create_token({ id: user.id, username: user.username, email: user.email }) }
+      user_object = { id: user.id, username: user.username, email: user.email }
+      render json: { token: Auth.create_token(user_object), profile: user_object }
     else
       render json: { errors: { message: 'Username or Password is incorrect' } }
     end
