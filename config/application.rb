ac691fa3da93bc5fdc66dc544f6c489da553ceea
Cernan Bernardo <cernanb@gmail.com>
2017-03-12 21:22:21 -0700

    configured rack cors middleware

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,12 @@ Bundler.require(*Rails.groups)
 module ReactRailsNotebookTradingPost
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
