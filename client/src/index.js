a7366b85379bb2ee87c0fdb49fac6448cd67cd87
Cernan Bernardo <cernanb@gmail.com>
2017-03-12 21:21:39 -0700

    started navigation bar with react router

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import About from './components/About'
+import Topics from './components/Topics'
+import Home from './components/Home'
+import Signup from './components/Signup'
+
+
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="/about" component={About}/>
+        <Route path="/signup" component={Signup}/>
+      </Route>
+  </Router>,
   document.getElementById('root')
 );
