f0a082427f2c08728f4db6e5dd30d6937c89de52
Cernan Bernardo <cernanb@gmail.com>
2017-04-09 21:46:27 -0700

    add user name to home component on successful login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {Component} from 'react'
 import AuthService from '../services/authService'
 import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
-const Home = (props) => (
-  <div>
-    {AuthService.isAuthenticated() ? <h1>Welcome {props.profile.username}</h1> : <h1>Please login or signup</h1>}
-  </div>
-)
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        {AuthService.isAuthenticated() ? <h1>Welcome {JSON.parse(localStorage.getItem('profile')).username} </h1> : <h1>Please <Link to='/login'>login</Link> or <Link to='signup'>signup</Link></h1>}
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
