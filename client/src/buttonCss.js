ab4a413bc2fd0653438bcdb3d8eca7b9475adecd
Cernan Bernardo <cernanb@gmail.com>
2017-04-12 23:30:36 -0700

    created css for button

---
+++ client/src/buttonCss.js
@@ -0,0 +1,14 @@
+import { css } from 'glamor'
+
+export default css({
+  'borderRadius': '50px',
+  border: '3px solid #EEE',
+  color: '#FFF',
+  padding: '5px 10px',
+  'fontWeight': '700',
+  'fontSize': '10px',
+  ':hover': {
+    background: '#AAA',
+    cursor: 'crosshair'
+  }
+})
