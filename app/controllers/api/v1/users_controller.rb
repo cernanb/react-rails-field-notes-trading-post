81f7ccee6ad5297d347398b783e35f21b33a0c27
Cernan Bernardo <cernanb@gmail.com>
2017-05-04 17:21:25 -0700

    updated signup action to include admin

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def signup
     user = User.new(user_params)
     if user.save
-      user_object = { id: user.id, username: user.username, email: user.email }
+      user_object = { id: user.id, username: user.username, email: user.email, admin: user.admin? }
       render json: { token: Auth.create_token(user_object), profile: user_object }
     else
       render json: { errors: { message: JSON.parse(user.errors.full_messages.to_json) } }
