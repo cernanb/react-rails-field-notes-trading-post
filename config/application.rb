68c27dae30ebdcb75f39567a05e97d39aa0d9974
cernanb <cernanb@gmail.com>
2019-07-02 22:37:39 -0700

    Update to Rails 5.2

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require "rails"
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -18,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module ReactRailsNotebookTradingPost
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
     config.autoload_paths << Rails.root.join('lib')
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
@@ -26,8 +29,9 @@ module ReactRailsNotebookTradingPost
       end
     end
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
