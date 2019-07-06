ce383c98baafc783316ef15003f2a91587f62a14
cernanb <cernanb@gmail.com>
2019-07-06 15:47:16 -0700

    Display users notebooks

--- client/src/components/UserNotebooks.js
+++ client/src/components/UserNotebooks.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
-import { getUserNotebooks } from '../actions/notebookActions'
-import { bindActionCreators } from 'redux'
-import AuthService from '../services/authService'
-import { connect } from 'react-redux'
-import { css } from 'glamor'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { css } from 'glamor';
+import { Link } from 'react-router-dom';
+import AuthService from '../services/authService';
+import { getUserNotebooks } from '../actions/notebookActions';
 
 const notebookCSS = css({
   width: '33%',
-})
+});
 
 const linkCSS = css({
   textDecoration: 'none',
@@ -18,70 +18,67 @@ const linkCSS = css({
   ':hover': {
     color: '#383A3F',
   },
-})
+});
 
-const imageCSS = css({})
+const imageCSS = css({});
 
 const notebookCardTitle = css({
   marginBottom: '-45px',
   position: 'relative',
-})
+});
 
 const notebookContainerCSS = css({
   display: 'flex',
   flexDirection: 'row',
   flexWrap: 'wrap',
-})
+});
 
 class UserNotebooks extends Component {
   componentWillMount() {
     if (!AuthService.isAuthenticated()) {
-      this.props.router.push('/login')
+      this.props.router.push('/login');
     }
   }
 
   componentDidMount() {
-    this.props.actions.getUserNotebooks()
+    this.props.actions.getUserNotebooks();
   }
 
   render() {
-    const { notebooks } = this.props
+    const { notebooks } = this.props;
+
+    if (!notebooks) {
+      return null;
+    }
     return (
       <div {...notebookContainerCSS}>
         {notebooks.map(notebook => (
           <div {...notebookCSS} key={notebook.id}>
             <Link {...linkCSS} to={{ pathname: `/notebooks/${notebook.id}` }}>
               <p {...notebookCardTitle}>{notebook.edition}</p>
-              <img
-                {...imageCSS}
-                src="https://fieldnotes.imgix.net/images/products/FNC-32-Lunacy-A.jpg?auto=format&fit=crop&h=360&ixlib=php-1.1.0&q=55&w=400&s=0332b5fe718344668674db1c08d05741"
-              />
+              <img {...imageCSS} src={notebook.photo_url} />
             </Link>
           </div>
         ))}
       </div>
-    )
+    );
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    actions: bindActionCreators(
-      {
-        getUserNotebooks,
-      },
-      dispatch
-    ),
-  }
-}
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    {
+      getUserNotebooks,
+    },
+    dispatch
+  ),
+});
 
-const mapStateToProps = state => {
-  return {
-    notebooks: state.notebooks,
-  }
-}
+const mapStateToProps = state => ({
+  notebooks: state.auth.profile.notebooks,
+});
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(UserNotebooks)
+)(UserNotebooks);
